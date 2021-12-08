14507aa3ed475e9ad495759c476390725b06811b
Ruihang Xia <waynestxia@gmail.com>
2021-12-08 23:06:52 +0800

    [wip]: cannot achieve without epoch gc
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/node_ptr.rs
+++ src/node_ptr.rs
@@ -1,10 +1,13 @@
 use std::ptr::NonNull;
 
-use crate::sync::{Alloc, AtomicPtr, AtomicUsize, Mutex, MutexGuard, Ordering};
+use loom::sync::atomic::AtomicIsize;
+
+use crate::sync::{Alloc, AtomicPtr, Mutex, MutexGuard, Ordering};
 
 crate struct NodePtr {
     ptr: AtomicPtr<NodeRc>,
     mutex: Mutex<()>,
+    phantom_rc: AtomicIsize,
 }
 
 impl NodePtr {
@@ -14,6 +17,7 @@ impl NodePtr {
         Self {
             ptr: AtomicPtr::new(ptr),
             mutex: Mutex::new(()),
+            phantom_rc: AtomicIsize::new(0),
         }
     }
 
@@ -21,9 +25,15 @@ impl NodePtr {
     ///
     /// The ref holds one reference counting, thus it is legal as long as it exist.
     crate fn read(&self) -> NodeRef {
+        let phantom_rc = self.phantom_rc.fetch_add(1, Ordering::SeqCst) + 1;
         let ptr = self.ptr.load(Ordering::SeqCst);
         unsafe {
-            (*ptr).increase();
+            NodeRc::accumulate_rc(ptr, 1);
+            self.phantom_rc.fetch_add(-1, Ordering::SeqCst);
+            // if self.phantom_rc.fetch_add(-1, Ordering::SeqCst) < phantom_rc {
+            //     NodeRc::accumulate_rc(ptr, -1);
+            //     self.phantom_rc.fetch_add(1, Ordering::SeqCst);
+            // }
             NodeRef {
                 ptr: NonNull::new_unchecked(ptr),
             }
@@ -44,7 +54,7 @@ impl Drop for NodePtr {
     fn drop(&mut self) {
         let ptr = self.ptr.load(Ordering::SeqCst);
         unsafe {
-            NodeRc::destroy(ptr);
+            NodeRc::accumulate_rc(ptr, -1);
         }
     }
 }
@@ -63,7 +73,7 @@ impl Drop for NodeRef {
     fn drop(&mut self) {
         let ptr = self.ptr.as_ptr();
         unsafe {
-            NodeRc::destroy(ptr);
+            NodeRc::accumulate_rc(ptr, -1);
         }
     }
 }
@@ -80,47 +90,46 @@ impl<'a> NodeRefMut<'a> {
 
     /// Replace source [NodePtr]'s pointer.
     crate fn replace<T>(&self, ptr: *mut T) {
-        let new_rc = NodeRc::new_boxed(NonNull::new(ptr as _).unwrap());
-        let replaced = self.ptr.ptr.swap(new_rc, Ordering::SeqCst);
+        let new_item = NodeRc::new_boxed(NonNull::new(ptr as _).unwrap());
+        let replaced = self.ptr.ptr.swap(new_item, Ordering::SeqCst);
+        // let phantom_rc = self.ptr.phantom_rc.swap(0, Ordering::SeqCst) - 1;
+        let phantom_rc = self.ptr.phantom_rc.load(Ordering::SeqCst);
         unsafe {
-            NodeRc::destroy(replaced);
+            if phantom_rc == 0 {
+                NodeRc::accumulate_rc(replaced, -1);
+            } else {
+                NodeRc::decrease_one(replaced);
+            }
         }
     }
 }
 
 crate struct NodeRc {
     ptr: NonNull<()>,
-    count: AtomicUsize,
+    count: AtomicIsize,
 }
 
 impl NodeRc {
     fn new_boxed(ptr: NonNull<()>) -> *mut Self {
         let rc = Self {
             ptr,
-            count: AtomicUsize::new(1),
+            count: AtomicIsize::new(1),
         };
         Box::into_raw(Box::new_in(rc, Alloc))
     }
 
-    fn increase(&self) {
-        self.count.fetch_add(1, Ordering::SeqCst);
-    }
-
-    /// Return `true` if it's the last reference.
-    fn decrease(&self) -> bool {
-        if self.count.fetch_sub(1, Ordering::SeqCst) <= 1 {
-            // todo: drop node
-            true
-        } else {
-            false
-        }
-    }
-
-    unsafe fn destroy(ptr: *mut Self) {
-        if (*ptr).decrease() {
-            println!("destroying {:?}", (*ptr).ptr);
+    unsafe fn accumulate_rc(ptr: *mut Self, rc: isize) {
+        let updated = (*ptr).count.fetch_add(rc, Ordering::SeqCst) + rc;
+        println!("{:?} accumulate rc {}, after: {}", (*ptr).ptr, rc, updated);
+        if updated < 1 {
+            println!("destroy after accumulate {:?}", (*ptr).ptr);
             Box::from_raw_in(ptr, Alloc);
         }
+    }
+
+    unsafe fn decrease_one(ptr: *mut Self) {
+        (*ptr).count.fetch_add(-1, Ordering::SeqCst);
+        println!("{:?} decrease one", (*ptr).ptr);
     }
 }
 
@@ -181,7 +190,91 @@ mod test {
     fn loom_alloc() {
         loom::model(|| {
             let rc = NodeRc::new_boxed(NonNull::new(0x1usize as *mut ()).unwrap());
-            unsafe { NodeRc::destroy(rc) };
+            unsafe { NodeRc::accumulate_rc(rc, -1) };
+        })
+    }
+
+    #[test]
+    fn single_write() {
+        loom::model(|| {
+            let node_ptr = NodePtr::new(0x1usize as *mut ());
+            let node = Arc::new(node_ptr);
+
+            let node_write = node.clone();
+            let writer = thread::spawn(move || {
+                let write_ref = node_write.write();
+                write_ref.replace(0x2usize as *mut ());
+
+                let ptr = write_ref.get();
+                let addr = ptr.as_ptr() as usize;
+                assert_eq!(addr, 0x2usize);
+            });
+
+            writer.join().unwrap();
+        })
+    }
+
+    #[test]
+    fn read_before_write_begin() {
+        loom::model(|| {
+            let node_ptr = NodePtr::new(0x1usize as *mut ());
+            let node = Arc::new(node_ptr);
+
+            let read_ref = node.read();
+            let reader = thread::spawn(move || {
+                println!("reader thread begin");
+                let ptr = read_ref.get();
+                let addr = ptr.as_ptr() as usize;
+                assert_eq!(addr, 0x1usize);
+                println!("reader thread finish");
+            });
+
+            let node_write = node.clone();
+            let writer = thread::spawn(move || {
+                println!("writer thread begin");
+                let write_ref = node_write.write();
+                write_ref.replace(0x2usize as *mut ());
+
+                let ptr = write_ref.get();
+                let addr = ptr.as_ptr() as usize;
+                assert_eq!(addr, 0x2usize);
+                println!("writer thread finish");
+            });
+
+            reader.join().unwrap();
+            writer.join().unwrap();
+        })
+    }
+
+    #[test]
+    fn read_after_write_begin() {
+        loom::model(|| {
+            let node_ptr = NodePtr::new(0x1usize as *mut ());
+            let node = Arc::new(node_ptr);
+
+            let node_write = node.clone();
+            let writer = thread::spawn(move || {
+                println!("writer thread begin");
+                let write_ref = node_write.write();
+                write_ref.replace(0x2usize as *mut ());
+
+                let ptr = write_ref.get();
+                let addr = ptr.as_ptr() as usize;
+                assert_eq!(addr, 0x2usize);
+                println!("writer thread finish");
+            });
+
+            let read_ref = node.read();
+            let reader = thread::spawn(move || {
+                println!("reader thread begin");
+                let ptr = read_ref.get();
+                let addr = ptr.as_ptr() as usize;
+                assert_eq!(addr, 0x1usize);
+                println!("reader thread finish");
+            });
+
+            writer.join().unwrap();
+            reader.join().unwrap();
         })
     }
 }
