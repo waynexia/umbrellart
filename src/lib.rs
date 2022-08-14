000000073214fd5c0c41877acd423f4c97b2f4d1
Ruihang Xia <waynestxia@gmail.com>
2022-08-14 21:53:32 +0800

    *: tensei!
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,1 @@
-#![feature(core_intrinsics)]
-#![feature(nonnull_slice_from_raw_parts)]
-#![feature(alloc_layout_extra)]
-#![feature(trait_alias)]
-#![feature(allocator_api)]
-#![allow(dead_code)]
 
-mod node;
-mod node_ptr;
-mod node_ref;
-mod serialized;
-mod sync;
-
-pub use node::*;
-// for fuzz test. should not export.
-pub use node_ref::NodeRef;
