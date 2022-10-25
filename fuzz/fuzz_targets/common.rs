8b81f6ecf18c3cd564bb194a65d1baacad688703
Ruihang Xia <waynestxia@gmail.com>
2022-10-26 01:09:30 +0800

    test: improve NodeOperation's debug format
    Signed-off-by: Ruihang Xia <waynestxia@gmail.com>

--- fuzz/fuzz_targets/common.rs
+++ fuzz/fuzz_targets/common.rs
@@ -1,7 +1,44 @@
 use libfuzzer_sys::arbitrary::Arbitrary;
 
-#[derive(Arbitrary, Debug)]
+#[derive(Arbitrary)]
 pub enum NodeOperation {
     Insert { key: Vec<u8>, value: u64 },
     Remove { remove_exist: bool, key: Vec<u8> },
+}
+
+impl NodeOperation {
+    fn convert_key(key: &[u8]) -> Vec<u8> {
+        let mut key: Vec<u8> = key.iter().filter(|byte| **byte != 0).copied().collect();
+        key.push(0);
+        key
+    }
+
+    pub fn canonicalize(&self) -> Vec<u8> {
+        match self {
+            NodeOperation::Insert { key, .. } => Self::convert_key(key),
+            NodeOperation::Remove { key, .. } => Self::convert_key(key),
+        }
+    }
+}
+
+impl std::fmt::Debug for NodeOperation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("art.")?;
+        let key = self.canonicalize();
+
+        match self {
+            NodeOperation::Insert { value, .. } => {
+                f.write_fmt(format_args!("insert(&{:?}, {});", key.as_slice(), value))?;
+            }
+            NodeOperation::Remove { remove_exist, .. } => {
+                if *remove_exist {
+                    f.write_fmt(format_args!("remove(&{:?});", key.as_slice()))?;
+                } else {
+                    f.write_fmt(format_args!("remove(&[]);"))?;
+                }
+            }
+        }
+
+        Ok(())
+    }
 }
